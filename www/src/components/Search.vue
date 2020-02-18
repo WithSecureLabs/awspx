940cf848188cbea15f0f97eea7930eda8a715601
Craig Koorn <12154702+beatro0t@users.noreply.github.com>
2020-02-18 19:37:24 +0200

    Add empty database helper

--- www/src/components/Search.vue
+++ www/src/components/Search.vue
@@ -1,106 +1,158 @@
 <template>
-  <div id="search">
-    <!-- Search expansion -->
-    <v-card
-      :style="{display: !show ? '' : 'none'}"
-      :height="(hover) ? 30: 20"
-      width="50"
-      class="mx-auto"
-      @click="show = !show"
-      @mouseover="hover = true"
-      @mouseleave="hover = false"
-    >
-      <div class="d-flex justify-center align-center">
-        <v-icon>mdi-chevron-up</v-icon>
-      </div>
-    </v-card>
+  <div>
+    <div v-if="loading || populated" id="search">
+      <!-- Search expansion -->
+      <v-card
+        :style="{display: !show ? '' : 'none'}"
+        :height="(hover) ? 30: 20"
+        width="50"
+        class="mx-auto"
+        @click="show = !show"
+        @mouseover="hover = true"
+        @mouseleave="hover = false"
+      >
+        <div class="d-flex justify-center align-center">
+          <v-icon>mdi-chevron-up</v-icon>
+        </div>
+      </v-card>
 
-    <!-- Autocomplete -->
-    <v-expand-transition>
-      <div :style="{display: show ? '' : 'none'}" class="mb-5">
-        <v-card>
-          <v-autocomplete
-            :filter="filter"
-            :items="(view == 0) ? resources : actions"
-            :loading="loading"
-            :cache-items="false"
-            :aria-autocomplete="false"
-            @input="search=null"
-            :search-input.sync="search"
-            v-model="selected"
-            attach="#search"
-            item-text="name"
-            item-value="id"
-            return-object
-            hide-details
-            hide-no-data
-            clearable
-            autofocus
-            multiple
-            :menu-props="{top: true, nudgeTop: 10}"
-          >
-            <template #append>
-              <v-tooltip top z-index="10">
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    x-small
-                    fab
-                    :color="(view === 1) ? 'none' : colors.Allow"
-                    @click="view = (view + 1 ) % 2"
-                    v-on="on"
-                    class="mt-n3 ml-n10"
-                  >
-                    <v-img width="25" :src="(view == 1) ? icons.AWS.Resource :  icons.AWS.Action" />
-                  </v-btn>
-                </template>
-                <span style="z-index: 1" v-if="view === 0">
-                  Switch to
-                  <b>Actions</b>
-                </span>
-                <span style="z-index: 1" v-else-if="view === 1">
-                  Switch to
-                  <b>Resources</b>
-                </span>
-              </v-tooltip>
-            </template>
-
-            <!-- Autocomplete selection -->
-            <template #selection="data">
-              <v-chip
-                v-on="data.on"
-                close
-                outlined
-                @click="data.select"
-                @click:close="selected = selected.filter(s => s.id !== data.item.id)"
-              >
-                <v-avatar outlined :color="(view === 1) ? colors[data.item.access] : 'white'">
-                  <v-img :src="icon(data.item)" />
-                </v-avatar>
-                <span class="ml-2" style="font-size: 11px">{{ data.item.name }}</span>
-              </v-chip>
-            </template>
-
-            <!-- Autocomplete items -->
-            <template #item="data">
-              <template v-on="data.on">
-                <v-list-item-avatar
-                  size="50"
-                  style="border: 1px solid #ccc; padding: 30px;"
-                  :color="(view === 1) ? colors[data.item.access] : 'none'"
-                >
-                  <img height="50" width="50" :src="icon(data.item)" />
-                </v-list-item-avatar>
-                <v-list-item-content style="text-align: center">
-                  <v-list-item-title v-html="highlight(data.item.name)"></v-list-item-title>
-                  <v-list-item-subtitle v-if="view === 0" v-html="data.item.id"></v-list-item-subtitle>
-                  <v-list-item-subtitle v-else-if="view === 1" v-html="data.item.description"></v-list-item-subtitle>
-                </v-list-item-content>
+      <!-- Autocomplete -->
+      <v-expand-transition>
+        <div :style="{display: show ? '' : 'none'}" class="mb-5">
+          <v-card>
+            <v-autocomplete
+              :filter="filter"
+              :items="(view == 0) ? resources : actions"
+              :loading="loading"
+              :cache-items="false"
+              :aria-autocomplete="false"
+              @input="search=null"
+              :search-input.sync="search"
+              v-model="selected"
+              attach="#search"
+              item-text="name"
+              item-value="id"
+              return-object
+              hide-details
+              hide-no-data
+              clearable
+              autofocus
+              multiple
+              :menu-props="{top: true, nudgeTop: 10}"
+            >
+              <template #append>
+                <v-tooltip top z-index="10">
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      x-small
+                      fab
+                      :color="(view === 1) ? 'none' : colors.Allow"
+                      @click="view = (view + 1 ) % 2"
+                      v-on="on"
+                      class="mt-n3 ml-n10"
+                    >
+                      <v-img
+                        width="25"
+                        :src="(view == 1) ? icons.AWS.Resource :  icons.AWS.Action"
+                      />
+                    </v-btn>
+                  </template>
+                  <span style="z-index: 1" v-if="view === 0">
+                    Switch to
+                    <b>Actions</b>
+                  </span>
+                  <span style="z-index: 1" v-else-if="view === 1">
+                    Switch to
+                    <b>Resources</b>
+                  </span>
+                </v-tooltip>
               </template>
-            </template>
-          </v-autocomplete>
+
+              <!-- Autocomplete selection -->
+              <template #selection="data">
+                <v-chip
+                  v-on="data.on"
+                  close
+                  outlined
+                  @click="data.select"
+                  @click:close="selected = selected.filter(s => s.id !== data.item.id)"
+                >
+                  <v-avatar outlined :color="(view === 1) ? colors[data.item.access] : 'white'">
+                    <v-img :src="icon(data.item)" />
+                  </v-avatar>
+                  <span class="ml-2" style="font-size: 11px">{{ data.item.name }}</span>
+                </v-chip>
+              </template>
+
+              <!-- Autocomplete items -->
+              <template #item="data">
+                <template v-on="data.on">
+                  <v-list-item-avatar
+                    size="50"
+                    style="border: 1px solid #ccc; padding: 30px;"
+                    :color="(view === 1) ? colors[data.item.access] : 'none'"
+                  >
+                    <img height="50" width="50" :src="icon(data.item)" />
+                  </v-list-item-avatar>
+                  <v-list-item-content style="text-align: center">
+                    <v-list-item-title v-html="highlight(data.item.name)"></v-list-item-title>
+                    <v-list-item-subtitle v-if="view === 0" v-html="data.item.id"></v-list-item-subtitle>
+                    <v-list-item-subtitle v-else-if="view === 1" v-html="data.item.description"></v-list-item-subtitle>
+                  </v-list-item-content>
+                </template>
+              </template>
+            </v-autocomplete>
+          </v-card>
+        </div>
+      </v-expand-transition>
+    </div>
+    <!-- Empty database helper -->
+    <v-stepper v-else style="width: 50vw;" class="mx-auto" value="2">
+      <v-stepper-header>
+        <v-stepper-step step="1" complete>Install awspx</v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step step="2">Populate database</v-stepper-step>
+        <v-card></v-card>
+        <v-divider></v-divider>
+        <v-stepper-step step="3">Explore</v-stepper-step>
+      </v-stepper-header>
+
+      <v-stepper-content step="2">
+        <v-card color="grey lighten-5" class="mb-5 pa-10">
+          <v-card-title>You may have jumped the gun...</v-card-title>
+          <v-card-subtitle class="mb-5">We couldn't find any data to work with</v-card-subtitle>
+          <v-card-text>
+            <div class="mb-2">
+              <b>Either</b> run the ingestor to load an account of your own:
+            </div>
+            <v-card style="font-size: 11px; border: 1px solid whitesmoke;" class="pa-2">
+              [root@localhost ~]#
+              <b>awspx ingest</b>
+              <br />[-] The profile 'default' was not found. Would you like to create it? (y/n) y
+              <br />AWS Access Key ID [None]: ****9XY7
+              <br />AWS Secret Access Key [None]: ****ks91
+              <br />Default region name [None]: eu-west-1
+              <br />Default output format [None]: json
+              <br />
+            </v-card>
+            <div class="mt-10 mb-2">
+              <b>OR</b> load an existing database (e.g. the provided sample):
+            </div>
+            <v-card style="font-size: 11px; border: 1px solid whitesmoke;" class="pa-2">
+              <br />[root@localhost ~]#
+              <b>awspx db --load-zip sample.zip</b>
+              <br />[*] Importing records from /opt/awspx/data/sample.zip
+              <br />
+              <br />[root@localhost ~]#
+              <b>awspx attacks</b>
+              <br />[*] Searching database for attack patterns
+              <br />
+            </v-card>
+          </v-card-text>
         </v-card>
-      </div>
-    </v-expand-transition>
+        <v-btn color="primary" class="my-auto" :loading="loading" @click="load()">Check again</v-btn>
+      </v-stepper-content>
+    </v-stepper>
   </div>
 </template>
 
@@ -122,7 +174,7 @@ export default {
     return {
       search: "",
       hover: false,
-      loading: false,
+      loading: true,
       items: [],
       view: 0,
       selected: [],
@@ -253,6 +305,9 @@ export default {
       set: function(value) {
         this.$emit("toggle", !value);
       }
+    },
+    populated: function() {
+      return this.resources.length + this.actions.length > 0;
     }
   },
   mounted() {
