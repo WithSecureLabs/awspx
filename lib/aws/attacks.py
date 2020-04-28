0f5b7faef1e36d798bec004195da0a343e278c95
Craig Koorn <12154702+beatro0t@users.noreply.github.com>
2020-04-28 14:09:07 +0200

    Update output

--- lib/aws/attacks.py
+++ lib/aws/attacks.py
@@ -1357,7 +1357,7 @@ class Attacks:
                           else 0 for s in stats])
 
         sys.stdout.write("\033[F\033[K")
-        print(f"[+] {discovered} patterns were discovered " + str(
+        print(f"[+] {discovered} potential attacks were discovered " + str(
               f"(successfully converged after {iteration} iterations)" if iteration < max_iterations else
               f"(failed to converge of {max_iterations})")
               )
